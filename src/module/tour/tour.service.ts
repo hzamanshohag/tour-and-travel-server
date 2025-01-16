eca9fa01f9bc5105cc1e35a99898d509e26163e0
Md Hasanuzzaman Shohag <hzaman.live@gmail.com>
2025-01-17 00:50:57 +0600

    tour search & sort added

--- src/module/tour/tour.service.ts
+++ src/module/tour/tour.service.ts
@@ -12,8 +12,51 @@ const createTour = async (payload: ITour) => {
   return result;
 };
 
-const getTours = async () => {
-  const result = Tour.find();
+const getTours = async (query: Record<string, unknown>) => {
+  const queryObj = { ...query };
+  const excludingImportant = [
+    'searchTerm',
+    'page',
+    'limit',
+    'sortOrder',
+    'sortBy',
+  ];
+  excludingImportant.forEach((key) => delete queryObj[key]);
+  console.log({ query }, { queryObj });
+
+  const searchTerm = query?.searchTerm || '';
+  const searchableFields = ['name', 'startLocation', 'locations'];
+  // {searchTerm=Base Camp}
+  // const searchQuery = await Tour.find({
+  //   $or: [
+  //     { name: { $regex: searchTerm, $options: 'i' } },
+  //     { startLocation: { $regex: searchTerm, $options: 'i' } },
+  //     { locations: { $regex: searchTerm, $options: 'i' } },
+  //   ],
+  // });
+  const searchableQuery = Tour.find({
+    $or: searchableFields.map((field) => ({
+      [field]: { $regex: searchTerm, $options: 'i' },
+    })),
+  });
+  // const result = await searchQuery.find(queryObj);
+  const filterQuery = searchableQuery.find(queryObj);
+
+  const page = Number(query?.page) || 1;
+  const limit = Number(query?.limit) || 10;
+  // skip = (page-1) * limit
+  const skiped = (page - 1) * limit;
+  const paginatedQuery = filterQuery.skip(skiped).limit(limit);
+
+  let sortStr = '-price';
+
+  if (query?.sortBy && query?.sortOrder) {
+    const sortBy = query?.sortBy;
+    const sortOrder = query?.sortOrder;
+    sortStr = `${sortOrder === 'desc' ? '-' : ''}${sortBy}`;
+  }
+  const result = await paginatedQuery.sort(sortStr);
+
   return result;
 };
 
@@ -33,7 +76,7 @@ const deleteTour = async (id: string) => {
 };
 
 const getNextSchedule = async (id: string) => {
-    console.log(id)
+  console.log(id);
   const tour = await Tour.getNextNearestStartDateAndEndData();
   //   const nextSchedule = tour?.getNextNearestStartDateAndEndData()
 
